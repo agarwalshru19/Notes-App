c9a026c4de7e93c8d3ba6d72acbac9b49632a231
agarwalshru19 <157809473+agarwalshru19@users.noreply.github.com>
2024-12-15 15:49:23 +0530

    Update verifyToken.js

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -4,9 +4,11 @@ dotenv.config();
 
 const verifyToken = async (req, res, next) => {
   try {
-
+    const jwtToken = req.cookies.access_token;
+    if (!jwtToken) {
       return res.status(403).json("Unauthorized");
     }
+
     const payload = jwt.verify(jwtToken, process.env.JWT_SECRET);
     // console.log("Decoded Payload: ", payload);
     req.user = payload.id; // Attach user ID to request object
@@ -19,4 +21,5 @@ const verifyToken = async (req, res, next) => {
 };
 
 export default verifyToken;
+
 
