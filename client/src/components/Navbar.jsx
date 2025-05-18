c6c6986495522f53d41007c9b8977b4c097cb934
agarwalshru19 <157809473+agarwalshru19@users.noreply.github.com>
2025-05-18 13:41:14 +0530

    Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
   const handleLogout = async () => {
     try {
       dispatch(signOutStart());
-      const res = await axios.get("https://notes-app-backend-b6wb.onrender.com/api/auth/logout", {
+      const res = await axios.get("https://notes-app-backend-puy9.onrender.com/api/auth/logout", {
         withCredentials: true,
       });
       console.log(res.data);
